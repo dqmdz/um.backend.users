a4c409cbca37f60479f8f096313f95e15651b420
dqmdz <daniel.quinterospinto@gmail.com>
2021-09-17 06:22:34 -0300

    Agregado de CORS

--- src/main/java/ar/edu/programacion/backend/users/model/User.java
+++ src/main/java/ar/edu/programacion/backend/users/model/User.java
@@ -6,6 +6,8 @@ package ar.edu.programacion.backend.users.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -29,6 +31,7 @@ public class User implements Serializable {
 	private static final long serialVersionUID = -1508200398263988124L;
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String name;
